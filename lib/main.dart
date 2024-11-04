e1d97aae7d4506b0012ad222c49f854b84a7bc0c
Sascha Ziegler <sascha8398@googlemail.com>
2024-11-04 15:01:08 +0100

    added Stopwatch Screen with 3 Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,54 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple.shade800,
+          title: const Text("Stopwatch"),
+          titleTextStyle: const TextStyle(
+              fontSize: 28, color: Colors.white, fontWeight: FontWeight.bold),
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                      style: TextStyle(
+                          fontSize: 56,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.deepPurple.shade800),
+                      'Sekunden'),
+                  const SizedBox(
+                    width: 24,
+                  ),
+                  Text(
+                      style: TextStyle(
+                          fontSize: 56,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.deepPurple.shade800),
+                      's'),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(onPressed: () {}, child: const Text("Start")),
+                  const SizedBox(
+                    width: 24,
+                  ),
+                  ElevatedButton(onPressed: () {}, child: const Text("Stop")),
+                  const SizedBox(
+                    width: 24,
+                  ),
+                  ElevatedButton(onPressed: () {}, child: const Text("Clear")),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
